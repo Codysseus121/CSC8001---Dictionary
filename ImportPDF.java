228bd70283555006bf7410aa26193d81ce2a9fb2
Unknown <dimitris.par72@gmail.com>
2018-07-26 23:00:46 +0100

    Completed PDBBox text extraction.

--- ImportPDF.java
+++ ImportPDF.java
@@ -1,8 +1,8 @@
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper; 
+import org.apache.fontbox.FontBoxFont;
 import java.io.File;
 import java.io.IOException;
-
 
 /**
  * Write a description of class ImportPDF here.
@@ -12,11 +12,26 @@ import java.io.IOException;
  */
 public class ImportPDF
 {
-    public static void main (String [] args)
+    public static void main (String [] args) throws IOException
     {
+        System.setProperty("sun.java2d.cmm", "sun.java2d.cmm.kcms.KcmsServiceProvider");
+        File file = new File(("C://users//Mitsos//Desktop//Suffix arrays.pdf")); 
+        PDDocument document = null;
+        try
+        {
+            document = PDDocument.load(file);
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            String text = pdfStripper.getText(document);
+            System.out.println(text);
+        }
+        finally
+        {
+            
+                document.close();
+           }
+        }
+
+        
     
-    File file = new File("path of the document") 
-PDDocument document = PDDocument.load(file);
     
-    }
 }
